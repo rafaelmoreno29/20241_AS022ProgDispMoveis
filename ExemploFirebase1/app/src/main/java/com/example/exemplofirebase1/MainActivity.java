c0a44f33d5321ca3f69338296155504cb035ff8f
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-04-01 22:39:49 -0300

    Aula 7 Firebase

---
+++ ExemploFirebase1/app/src/main/java/com/example/exemplofirebase1/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.exemplofirebase1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseFirestore db;
+    FloatingActionButton buttonAdd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+        buscarUsuarios();
+        buttonAdd = (FloatingActionButton)findViewById(R.id.buttonAdd);
+        buttonAdd.setOnClickListener(v -> {
+            // Create a new user with a first and last name
+            Map<String, Object> user = new HashMap<>();
+            user.put("nome", "Rafael");
+            user.put("sobrenome", "Moreno");
+            user.put("anoNascimento", 1985);
+
+            // Add a new document with a generated ID
+            db.collection("usuarios")
+                    .add(user)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("TAG", "Error adding document", e);
+                        }
+                    });
+        });
+    }
+
+    public void buscarUsuarios(){
+        db.collection("usuarios")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("Usuario", document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w("Usuario", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
+}
